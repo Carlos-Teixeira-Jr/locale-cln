2636f64d7b2a3481d52c1adc98aecb934577c928
anagdinizp <anagabriela0399@hotmail.com>
2023-10-03 16:18:06 -0300

    feat: creation of favorite and unfavorite icon components for the displayed property information

--- components/organisms/propertyInfo/PropertyInfo.tsx
+++ components/organisms/propertyInfo/PropertyInfo.tsx
@@ -2,7 +2,12 @@ import clipboardy from 'clipboardy';
 import { useRouter } from 'next/router';
 import React, { MouseEvent, useState } from 'react';
 import 'react-tooltip/dist/react-tooltip.css';
-import { IMetadata, IPrices } from '../../../common/interfaces/property/propertyData';
+import {
+  IMetadata,
+  IPrices,
+} from '../../../common/interfaces/property/propertyData';
+import FavouritedIcon from '../../atoms/icons/favouritedIcon';
+import UnfavouritedIcon from '../../atoms/icons/unfavouritedIcon';
 import CalculatorModal from '../../atoms/modals/calculatorModal';
 import LinkCopiedTooltip from '../../atoms/tooltip/Tooltip';
 
@@ -152,27 +157,7 @@ const PropertyInfo: React.FC<IPropertyInfo> = ({ propertyID }: any) => {
             onClick={handleFavouriteBtnClick}
           >
             <p className="my-auto pr-4">Favoritar</p>
-            {favourited ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="48"
-                viewBox="0 96 960 960"
-                width="48"
-                fill="#F7F7F6"
-              >
-                <path d="m480 935-41-37q-106-97-175-167.5t-110-126Q113 549 96.5 504T80 413q0-90 60.5-150.5T290 202q57 0 105.5 27t84.5 78q42-54 89-79.5T670 202q89 0 149.5 60.5T880 413q0 46-16.5 91T806 604.5q-41 55.5-110 126T521 898l-41 37Z" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="48"
-                viewBox="0 96 960 960"
-                width="48"
-                fill="#F7F7F6"
-              >
-                <path d="m480 935-41-37q-105.768-97.121-174.884-167.561Q195 660 154 604.5T96.5 504Q80 459 80 413q0-90.155 60.5-150.577Q201 202 290 202q57 0 105.5 27t84.5 78q42-54 89-79.5T670 202q89 0 149.5 60.423Q880 322.845 880 413q0 46-16.5 91T806 604.5Q765 660 695.884 730.439 626.768 800.879 521 898l-41 37Zm0-79q101.236-92.995 166.618-159.498Q712 630 750.5 580t54-89.135q15.5-39.136 15.5-77.72Q820 347 778 304.5T670.225 262q-51.524 0-95.375 31.5Q531 325 504 382h-49q-26-56-69.85-88-43.851-32-95.375-32Q224 262 182 304.5t-42 108.816Q140 452 155.5 491.5t54 90Q248 632 314 698t166 158Zm0-297Z" />
-              </svg>
-            )}
+            {favourited ? <FavouritedIcon /> : <UnfavouritedIcon />}
           </button>
         </div>
 
