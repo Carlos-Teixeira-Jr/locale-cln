845d5df32d7ccf44dc041e5e32c91db276130949
unknown <teixeirahist1988@gmail.com>
2024-03-15 19:20:30 -0300

    feat: credit card component adjusted

--- hooks/modifyStrig.tsx
+++ hooks/modifyStrig.tsx
@@ -14,6 +14,34 @@ export default function modifyString(str: string) {
   if (str === '/property/[id]') {
     str = '/Imóvel'
   }
+  if (str === '/adminNotifications') {
+    str = '/notificações'
+  }
+  if (str === '/adminMessages') {
+    str = '/mensagens'
+  }
+  if (str === '/privacyPolicies') {
+    str = '/políticas de Privacidade'
+  }
+  if (str === '/register') {
+    str = '/anunciar - Etapa 1'
+  }
+  if (str === '/registerStep2') {
+    str = '/anunciar - Etapa 2'
+  }
+  if (str === '/registerStep3') {
+    str = '/anunciar - Etapa 3'
+  }
+  if (str === '/registerStep35') {
+    str = '/anunciar'
+  }
+  if (str === '/registerStep4') {
+    str = '/parabéns!'
+  }
+  if (str === '/announcement') {
+    str = '/planos de Anúncio'
+  }
+
 
   const firstCharRemoved = str.substring(1);
   const secondCharCapitalized = firstCharRemoved.charAt(0).toUpperCase();
