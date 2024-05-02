3847849a1529e97a788b8f93b7fe4739f07708be
unknown <teixeirahist1988@gmail.com>
2024-05-02 15:41:58 -0300

    fix: fixing logic on change plan system

--- components/atoms/modals/galleryModal.tsx
+++ components/atoms/modals/galleryModal.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { IData } from '../../../common/interfaces/property/propertyData';
+import { useIsMobile } from '../../../hooks/useIsMobile';
 import Loading from '../loading';
 export interface IGalleryModal {
   setModalIsOpen: (isOpen: boolean) => void;
@@ -20,6 +21,7 @@ const GalleryModal: React.FC<IGalleryModal> = ({
   const [currentIndex, setCurrentIndex] = useState(selectedImage);
   const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
   const [isLoading, setIsLoading] = useState(true);
+  const isMobile = useIsMobile();
 
   // Função para carregar a imagem e obter suas dimensões
   useEffect(() => {
@@ -44,16 +46,16 @@ const GalleryModal: React.FC<IGalleryModal> = ({
   };
 
   const nextImage = () => {
-    const isLastImage = currentIndex === property.images.length - 1;
+    const isLastImage = currentIndex === property.images.length;
     const newIndex = isLastImage ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
   return (
-    <div className={`right-0 left-0 top-0 my-auto pb-12 bg-black/90 fixed z-[214748364] group inset-x-0 overflow-x-hidden overflow-y-hidden overflow-hidden ${imageDimensions.height < 480 ?
+    <div className={`right-0 left-0 top-0 my-auto pb-12 bg-black/90 fixed z-[214748364] group inset-x-0 overflow-x-hidden overflow-y-hidden overflow-hidden transition-opacity duration-700 ${imageDimensions.height < 480 ?
       'md:h-full' :
       'h-full'
-      } transition-opacity duration-700 ${!modalIsOpen ? 'opcaity-0 hidden' : 'opacity-100 visible'}`}>
+      } ${!modalIsOpen ? 'opcaity-0 hidden' : 'opacity-100 visible'}`}>
       <div>
         <AiOutlineClose
           className="hidden group-hover:block absolute top-[4%] md:top-[5%] -translate-x-0 -translate-y-[50%] right-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
@@ -72,22 +74,22 @@ const GalleryModal: React.FC<IGalleryModal> = ({
             alt=""
             width={imageDimensions.width}
             height={imageDimensions.height}
-            className="rounded-3xl md:mx-5 lg:mx-0 max-h-full object-contain"
+            className="rounded-3xl md:mx-5 lg:mx-0 max-h-full object-contain px-2 md:px-0"
           />
         )}
       </div>
       <div>
         <BsChevronCompactLeft
           onClick={prevImage}
-          className="hidden group-hover:block absolute top-[40%] md:top-[50%] -translate-x-0 -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-          size={50}
+          className="hidden group-hover:block absolute top-[90%] md:top-[50%] -translate-x-0 -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          size={isMobile ? 40 : 50}
         />
       </div>
       <div>
         <BsChevronCompactRight
           onClick={nextImage}
-          className="hidden group-hover:block absolute top-[40%] md:top-[50%] -translate-x-0 -translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-          size={50}
+          className="hidden group-hover:block absolute top-[90%] md:top-[50%] -translate-x-0 -translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          size={isMobile ? 40 : 50}
         />
       </div>
       <div className="flex top-4 justify-center py-2">
